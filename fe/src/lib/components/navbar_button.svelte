b95761a3577d6ce369a168dccfe9b38467b9c3fd
Drajad-Kusuma-Adi <drajadkusumaadi@gmail.com>
2024-07-24 13:57:52 +0700

    fix: scroll to top when moving between pages

--- fe/src/lib/components/navbar_button.svelte
+++ fe/src/lib/components/navbar_button.svelte
@@ -9,7 +9,7 @@
   import { ripple } from 'svelte-ripple-action';
 </script>
 
-<button use:ripple class={`rounded-full size-16 p-4 mx-1 ${isActive ? `bg-french-violet text-white`: ``} flex justify-center items-center flex-col active:scale-90 transition duration-300`} on:click={() => sessionPage.set(option)}>
+<button use:ripple class={`rounded-full size-16 p-4 mx-1 ${isActive ? `bg-french-violet text-white`: ``} flex justify-center items-center flex-col active:scale-90 transition duration-300`} on:click={() => { sessionPage.set(option); window.scrollTo(0, 0); }}>
   <img src={options.includes(option) ? `/src/lib/assets/${option}${isActive ? `active` : ``}.svg` : ``} alt={option} class="size-6 mb-1">
   <p class="text-sm">{option.charAt(0).toUpperCase() + option.slice(1)}</p>
 </button>
