e214420a2a2cb9a2b2e122799fcb3557356f03dc
Drajad-Kusuma-Adi <drajadkusumaadi@gmail.com>
2024-07-24 11:12:42 +0700

    chore: add api utility

---
+++ fe/src/lib/utils/api.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8000/api/v1",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "multipart/form-data",
+  }
+});
+
+export const store = "http://localhost:8000/storage/uploads";
